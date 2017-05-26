1b5ec2dd39f5f08ae8312d544a82a21caba1620e
Benjamin Merot <bem.its@dsg.dk>
2017-05-26 14:49:49 +0200

    Adding handling of nexthop and service destination for OVH API call

--- files/ovh_ip_move.py
+++ files/ovh_ip_move.py
@@ -21,11 +21,12 @@ import ovh
 import sys
 
 
-def move(api_endpoint, app_key, app_secret, con_key, ip_failover, ip_dest):
+def move(api_endpoint, app_key, app_secret, con_key, ip_failover, ip_dest,
+         to_service):
     client = ovh.Client(endpoint=api_endpoint, application_key=app_key,
                         application_secret=app_secret, consumer_key=con_key)
 
-    result = client.post('/ip/' + ip_failover + '/move', to=ip_dest)
+    result = client.post('/ip/' + ip_failover + '/move', nexthop=ip_dest, to=to_service)
 
     print('Moved OVH IP failover')
 
@@ -36,7 +37,9 @@ if __name__ == "__main__":
     parser.add_argument('--app_secret', dest='app_secret', help='Application Secret for OVH API', required=True)
     parser.add_argument('--consumer_key', dest='con_key', help='Consumer key for OVH API', required=True)
     parser.add_argument('--ip_failover', dest='ip_failover', help='IP failover to manage', required=True)
-    parser.add_argument('--ip_destination', dest='ip_destination', help='Destination IP to route to', required=True)
+    parser.add_argument('--ip_destination', dest='ip_destination', help='Destination IP to route to (nexthop)')
+    parser.add_argument('--to_service', dest='to_service', help='Destination service to route to', required=True)
     parser.add_argument('--api_endpoint', dest='api_endpoint', default='ovh-eu', help='OVH API endpoint')
     args = parser.parse_args()
-    move(args.api_endpoint, args.app_key, args.app_secret, args.con_key, args.ip_failover, args.ip_destination)
+    move(args.api_endpoint, args.app_key, args.app_secret, args.con_key, args.ip_failover, args.ip_destination,
+         args.to_service)
